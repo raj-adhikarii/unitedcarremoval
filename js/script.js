70d3514b009721b3f36b28a1f2a140d0b81dfff1
raj-adhikarii <rajadhikari777.ra@gmail.com>
2022-12-14 16:56:22 +0545

    Working on inner pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// nav
+window.onscroll= ()=>{
+  scrollFunction()
+}
+
+function scrollFunction(){
+  if(document.body.scrollTop>1 || document.documentElement.scrollTop>1){
+    document.getElementById("navbar").style.position = "fixed";
+    document.getElementById("navbar").style.background = "#f5f5f5";
+    document.getElementById("navbar").style.left = "0";
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.width = "100%";
+  } else {
+    document.getElementById("navbar").style.position = "relative";
+    // document.getElementById("navbar").style.top = "38";
+  }
+}
 var acc = document.getElementsByClassName("accordion");
 var i;
 
