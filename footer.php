78227e1c378c1d31683a209ed1ddea00df751dfb
raj-adhikarii <rajadhikari777.ra@gmail.com>
2022-12-26 17:00:05 +0545

    Theme development started

---
+++ footer.php
@@ -0,0 +1,72 @@
+ <!-- Footer -->
+ <footer class="main-footer hidden">
+            <div class="container">
+                <div class="footer-col-wrapper">
+                    <div class="footer-col">
+                        <h4 class="widget-title">
+                            Quick links
+                        </h4>
+                        <ul class="footer-list">
+                            <li class="footer-menu-link"><a href="/index.html">Home</a></li>
+                            <li class="footer-menu-link"><a href="/about.html">About</a></li>
+                            <li class="footer-menu-link"><a href="/scrap-car-removal.html">Scrap Car Removal</a></li>
+                            <li class="footer-menu-link"><a href="/sell-car-for-cash.html">Sell Car For Cash</a></li>
+                            <li class="footer-menu-link"><a href="/contact.html">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4 class="widget-title">
+                            Explore
+                        </h4>
+                        <ul class="footer-list">
+                            <li class="footer-menu-link"><a href="/car-wreckers.html">Car Wreckers</a></li>
+                            <li class="footer-menu-link"><a href="/location.html">Location</a></li>
+                            <li class="footer-menu-link"><a href="/privacy-policy.html">Privacy Policy</a></li>
+                            <li class="footer-menu-link"><a href="/terms-of-use.html">Terms Of Use</a></li>
+                            <li class="footer-menu-link"><a href="#">Sitemap</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4 class="widget-title">
+                            Our Services
+                        </h4>
+                        <ul class="footer-list">
+                            <li class="footer-menu-link"><a href="/used-car-removal.html">Used Car Removal</a></li>
+                            <li class="footer-menu-link"><a href="/cash-for-unwanted-car.html">Cash For Unwanted  Cars</a></li>
+                            <li class="footer-menu-link"><a href="/cash-for-scrap-car.html">Cash For Scrap  Cars</a></li>
+                            <li class="footer-menu-link"><a href="/cash-for-abandoned-car.html">Cash For Abandoned  Cars</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4 class="widget-title">
+                           Contact Us
+                        </h4>
+                        <ul class="footer-list">
+                            <li class="footer-menu-link"><a href="#">0487002222</a></li>
+                            <li class="footer-menu-link"><a href="#">info@unitedcarremoval.com.au</a></li>
+                            <li class="footer-menu-link"><a href="#">59 Lisbon St, Fairfield East, NSW, 2165</a></li>
+                            <li class="footer-menu-link"><a href="#">59 Wallsend Rd, Sandgate NSW 2304</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <section class="copyright">
+            <div class="container">
+                <div class="copyright-wraper">
+                    <div class="copyright-text">
+                        <p>© 2022 All Rights Reserved.</p>
+                    </div>
+                    <div class="social">
+                        <ul>
+                            <li class="social-icon"><a href="#"><img src="/img/facebook.svg" alt="facebook"></a></li>
+                            <li class="social-icon"><a href="#"><img src="/img/twiller.svg" alt="twitter"></a></li>
+                            <li class="social-icon"><a href="#"><img src="/img/insta.svg" alt="Instagram"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+</body>
+</html>
+<?php wp_footer(); ?>
